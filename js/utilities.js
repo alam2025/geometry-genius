625f77d04bc277d41e53f5a4573648054c67ec61
alam2025 <alam13100852@gmail.com>
2023-02-18 22:04:39 +0600

    completed

--- js/utilities.js
+++ js/utilities.js
@@ -2,13 +2,13 @@
 function getValue(id) {
       const field = document.getElementById(id);
       const fieldValue = parseFloat(field.value);
-      if (isNaN(fieldValue) || fieldValue<0) {
+      if (isNaN(fieldValue) || fieldValue < 0) {
             alert('Enter a real number .');
             field.value = '';
             return;
       }
       else {
-            field.value='';
+            field.value = '';
             return fieldValue;
       }
 }
@@ -20,44 +20,44 @@ function areaCalculate(factor, a, b) {
 }
 
 //set result
-function setResult(result,name) {
-      if(isNaN(result)){
+function setResult(result, name) {
+      if (isNaN(result)) {
             //nothing is show because resalut is nan
 
       }
-      else{
+      else {
             const resultSection = document.getElementById('result-section');
-      const mainResult = document.getElementById('result');
+            const mainResult = document.getElementById('result');
 
-      const div = document.createElement('div');
-      const node = document.querySelectorAll('#result div');
+            const div = document.createElement('div');
+            const node = document.querySelectorAll('#result div');
 
-      const geometryName= document.getElementById(name).innerText;
-      let finalResult= result.toFixed(2);
-      
-//set elememt in the result section
+            const geometryName = document.getElementById(name).innerText;
+            let finalResult = result.toFixed(2);
 
-      div.innerHTML = `
+            //set elememt in the result section
+
+            div.innerHTML = `
             <h2 class=" result-value flex gap-2 p-1 text-sm"> 
                   <span>${node.length + 1} </span>
                   
                   <span>${geometryName}</span>
-                  <span>${finalResult}</span>
+                  <span class="result-element">${finalResult}</span>
 
                   
                   <span> cm<sup>2</sup></span> 
-                  <button class="btn-convert text-xs ">Convert to m<sup>2</sup> 
+                  <button id="convert-${node.length+1}" class="btn-convert text-xs ">Convert to <span>m</span><sup>2</sup> 
                   </button>
              </h2><br>
      `;
 
-      mainResult.appendChild(div);
+            mainResult.appendChild(div);
       }
 }
 
 
 //set element value 
 
-function setElementValue(value, destination){
-      destination.innerText=value;
+function setElementValue(value, destination) {
+      destination.innerText = value;
 }
