c4cd845063848b073e9499b15f79ca3cc3693d46
alam2025 <alam13100852@gmail.com>
2023-02-18 08:15:41 +0600

    Interface create

---
+++ js/random color.js
@@ -0,0 +1,13 @@
+// const setBg = () => {
+//       const randomColor = Math.floor(Math.random()*16777215).toString(16);
+//       document.body.style.backgroundColor = "#" + randomColor;
+//       color.innerHTML = "#" + randomColor;
+//     }
+    
+//     genNew.addEventListener("click", setBg);
+//     setBg();
+
+    const s=document.querySelectorAll('#geometry-section>div');
+    for(i=0;i<s.length;i++){
+      console.log(s[i]);
+    }
